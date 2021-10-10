1fe943b2b356e4443208121c7c5f7007b94bc44e
Itawnya Walker <itawnya.walker@gmail.com>
2021-10-09 19:17:24 -0500

    Edited tic-tac-toe.js - Part 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,29 @@
 function main(){
 	window.onload = function() {load_board()}
+	//X_O(); 
 }
 
 function load_board(){
+	let move = "O";
 	let square = document.getElementById("board").getElementsByTagName('div');
 	for (let i = 0; i< square.length; i++){
-		square[i].classList.add("square");
+		square[i].classList.add("square"); 
+		
+		square[i].onclick = function() {
+			if (square[i].innerHTML == "" && move == "O"){
+				square[i].classList.add("O");
+				square[i].innerHTML = "O";
+				move = "X";
+			}
+			else if (square[i].innerHTML == "" && move == "X"){
+				square[i].classList.add("X");
+				square[i].innerHTML = "X";
+				move = "O";
+			}
+		}
 	}
 	console.log("board loaded");
 }
 
-main();
+
+main()
