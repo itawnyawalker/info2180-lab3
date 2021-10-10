7fa9af4438c27f0c1a76e8bea509a025fd9cf922
Itawnya Walker <itawnya.walker@gmail.com>
2021-10-09 19:25:17 -0500

    Edited tic-tac-toe.js - Part 3

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,6 @@
 function main(){
 	window.onload = function() {load_board()}
-	//X_O(); 
+	
 }
 
 function load_board(){
@@ -9,13 +9,16 @@ function load_board(){
 	for (let i = 0; i< square.length; i++){
 		square[i].classList.add("square"); 
 		
+		square[i].onmouseover = function() {square[i].classList.add("hover")}
+		square[i].onmouseout = function() {square[i].classList.remove("hover")}
+		
 		square[i].onclick = function() {
 			if (square[i].innerHTML == "" && move == "O"){
 				square[i].classList.add("O");
 				square[i].innerHTML = "O";
 				move = "X";
 			}
-			else if (square[i].innerHTML == "" && move == "X"){
+			else {
 				square[i].classList.add("X");
 				square[i].innerHTML = "X";
 				move = "O";
